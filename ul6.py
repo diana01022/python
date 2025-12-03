9510019a6213d1415e8fde27792f682c2074bb34
diana <diana.bohutska@ametikool.ee>
2025-12-03 13:53:30 +0200

    yl6

---
+++ ul6.py
@@ -0,0 +1,16 @@
+# Kirjuta programm, mis leiab kolmest kasutaja poolt sisestatud arvust maksimumi (ära kasuta max funktsiooni). 
+# (loogikatehted - logic operators)
+
+a = int(input("a: "))
+b = int(input("b: "))
+c = int(input("c: "))
+
+print(a, b, c)
+if a > b and a > c:
+    print("a is maximum") 
+
+elif b > c:
+    print("b is maximum")
+    
+else:
+    print("c is maximum")
